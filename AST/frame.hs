920f8307ecdeb898cdb055ccdfc28c6cfe32a7f6
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2016-02-15 20:56:16 +0100

    Add small AST for functions

--- AST/frame.hs
+++ AST/frame.hs
@@ -11,3 +11,30 @@ data Exp = Application Exp Exp |
 
 eval :: Program -> IO ()
 eval p = undefined
+
+-- A function consists of a name (String),
+-- a set of input arguments (Arg),
+-- a return type (Type) and a body (Body)
+-- todo add function body
+data Func = Function String Arg Type
+    deriving Show
+
+-- Arguments
+data Arg = Con Type Arg | Nil
+    deriving Show
+
+-- Example types ..
+data Type = Int | Double | Boolean | Char
+    deriving Show
+
+
+-- A quick parse-function for our function type
+parseF :: Func -> String
+parseF (Function s a t) = "function " ++ s
+                             ++ " : "
+                             ++ parseA a
+                             ++ (show t)
+    where
+        parseA a = case a of
+            Nil         -> ""
+            (Con t a')  -> (show t) ++ " -> " ++ parseA a'
