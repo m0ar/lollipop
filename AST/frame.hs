792e98efe2e0e5edcc17f08c55662c7a7a73da80
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2016-02-18 15:57:21 +0100

    Add Lambdas to expr and refactored AST

--- AST/frame.hs
+++ AST/frame.hs
@@ -6,29 +6,18 @@ type Program = [Declaration]
 data Declaration = DFun Func
                     | DConst String Exp
 
-data Exp = Application Exp Exp
-           | Var String
-           | SLit String
-           | Add Exp Exp
-           | Mult Exp Exp
-           | Print Exp
+data Exp = EApp Exp Exp
+           | EVar Var
+           | ELit Lit
+           | EAdd Exp Exp
+           | EMult Exp Exp
+           | EPrint Exp
+           | ELam Var Exp
 
-instance Show Exp where
-    show e = case e of
-        Application e1 e2 -> ""
-        Var s             -> s
-        SLit s            -> s
-        Add e1 e2         -> show e1 ++ " + " ++ show e2
-        Mult e1 e2        -> show e1 ++ " * " ++ show e2
+type Var = String
 
--- [DConst "main" (Appl (Var "print") (SLit "hello"))]
-
-eval :: Program -> IO ()
-eval p = undefined
-
-evalFunc :: Func -> IO ()
-evalFunc (Function _ _ _ (Body _ e)) = case e of
-            Print e'         -> putStrLn (show e')
+-- A list of variables to be used in function bodies
+type Vars = [Var]
 
 -- A function consists of a name (String),
 -- a set of input arguments (Arg),
@@ -37,27 +26,48 @@ evalFunc (Function _ _ _ (Body _ e)) = case e of
 data Func = Function String Arg Type Body
 
 -- Arguments
-data Arg = Con Type Arg | ANil
+data Arg = Con Type Arg | Nil
 
 -- Function body consists of
 -- a set of variables (Vars) and
 -- the statement/command (Stat) to be performed
 data Body = Body Vars Exp
 
--- A set of variables to be used in function bodies
-data Vars = Variable String Vars | VNil
-
 -- Example types ..
 data Type = Int | Double | Boolean | Char | IO | Void
     deriving Show
 
+data Value =
 
+data Lit = SLit String
+            | ILit Int
+            | DLit Double
+            | BLit Bool
+    deriving Show
+
+-- [DConst "main" (Appl (Var "print") (SLit "hello"))]
+
+eval :: Program -> IO ()
+eval p = undefined
+
+evalFunc :: Func -> IO ()
+evalFunc (Function _ _ _ (Body _ e)) = case e of
+            EPrint e'         -> putStrLn (show e')
 
 readExpr :: String -> Maybe Func
 readExpr s = Nothing
 
 
 -- Show functions --
+
+
+instance Show Exp where
+    show e = case e of
+        EApp e1 e2 -> ""
+        EVar s             -> s
+        ELit l             -> show l
+        EAdd e1 e2         -> show e1 ++ " + " ++ show e2
+        EMult e1 e2        -> show e1 ++ " * " ++ show e2
 
 instance Show Func where
     show (Function e a t b) = "function " ++ e
@@ -69,16 +79,11 @@ instance Show Func where
                                  ++ show b
 instance Show Arg where
     show a = case a of
-        ANil     -> ""
+        Nil      -> ""
         Con t a' -> (show t) ++ " -> " ++ show a'
 
 instance Show Body where
     show (Body vs c) = " "
-                        ++ show vs
-                        ++ "= "
-                        ++ show c
-
-instance Show Vars where
-    show var = case var of
-        VNil            -> ""
-        (Variable s vs) -> s ++ " " ++ show vs
+                    ++ show vs
+                    ++ "= "
+                    ++ show c
