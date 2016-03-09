4700fc384e41aafbbe9c2e409b1ff851a8a1e5ef
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2016-03-09 15:08:15 +0100

    Add support for conversion of arguments

--- Converter.hs
+++ Converter.hs
@@ -39,6 +39,17 @@ argToVar (A.DArg2 (A.Id name)) = name
 getArgs :: [A.Def] -> D.Vars
 getArgs = undefined
 
+cArg :: A.Arg -> D.Exp
+cArg (A.DArg1 lit) = case lit of
+    (A.LitInt x)    -> D.ELit (D.ILit (fromInteger x))
+    (A.LitDouble x) -> D.ELit (D.DLit x)
+    (A.LitChar x)   -> D.ELit (D.CLit x)
+cArg (A.DArg2 (A.Id name)) = D.EVar name
+cArg (A.DArg3 t) = case t of
+    (A.TTypeId (TypeId name))     -> D.EConstr name
+    (A.TLiTypeId (LiTypeId name)) -> D.EConstr name
+    -- (LitString x) ->
+
 cLit :: A.Literal -> D.Lit
 cLit (A.LitInt x)      = D.ILit $ fromInteger x
 cLit (A.LitDouble x)   = D.DLit x
@@ -46,12 +57,12 @@ cLit (A.LitChar x)     = D.CLit x
 cLit (A.LitString x)   = D.SLit x
 
 cExp :: A.Exp -> D.Exp
-cExp (A.EVar (Id name))   = (D.EVar name)
+cExp (A.EVar (A.Id name))   = (D.EVar name)
 cExp (A.ELiteral lit)     = (D.ELit $ cLit lit)
 -- cExp (A.ELet vID)       = (D.ELetIn )
 cExp (A.EApp e1 e2)       = (D.EApp (cExp e1) (cExp e2))
 cExp (A.EAdd e1 e2)       = (D.EAdd (cExp e1) (cExp e2))
-cExp (A.EAbs (Id name) e) = (D.ELam name (cExp e))
+cExp (A.EAbs (A.Id name) e) = (D.ELam name (cExp e))
 
 cGuard :: A.Guards -> D.Exp
 cGuard (A.DGuards1 e1 e2 gs) = D.ECase (cExp e2) [(D.Constr "True" [] (cExp e1)),
