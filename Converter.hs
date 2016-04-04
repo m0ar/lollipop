dd7ad4c3e3d889f906f8cbe68be1277168b1e27c
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2016-04-04 17:18:52 +0200

    Rework patterns in converter after reworked grammar

--- Converter.hs
+++ Converter.hs
@@ -60,9 +60,9 @@ allDef (d:ds) = case d of
 -- translates guards into equal case-expressions
 cGuard :: A.Guards -> A.Exp
 cGuard (A.DGuards1 e1 e2 gs) = cGuard (A.DGuards2 e1 e2 gs)
-cGuard (A.DGuards2 e1 e2 gs) = A.ECase e2 (A.ECases2 (A.PPat (A.PConst (A.DConst1 (A.TypeId "True")))) e1 (cGuard' gs))
-    where cGuard' (A.DGuards2 e1 e2 gs) = (A.ECases2 (A.PPat (A.PConst (A.DConst1 (A.TypeId "False")))) (cGuard gs) A.ECases3)
-cGuard (A.DExpGuard e)       = A.ECase e ((A.ECases2 (A.PPat (A.Pwild))) e (A.ECases3))
+cGuard (A.DGuards2 e1 e2 gs) = A.ECase e2 (A.ECases2 (A.PConstrEmp (TypeId "True")) e1 (cGuard' gs))
+    where cGuard' (A.DGuards2 e1 e2 gs) = (A.ECases2 (A.PConstrEmp (TypeId "False")) (cGuard gs) A.ECases3)
+cGuard (A.DExpGuard e)       = A.ECase e ((A.ECases2 (A.PWild)) e (A.ECases3))
                             -- last one is "otherwise"-case
 
 -- list of generated variables to introduce in declaration
@@ -72,41 +72,25 @@ variables = map (("#x"++).show) [1..]
 -- converts args to pat in DataTypes.hs
 -- where Pat = PLit Lit | PWild | PVar Var
 argToPat :: A.Arg -> D.Pattern
-argToPat (A.DArg p) = case p of
-    A.PTuplePat (A.TPattern ps) -> case length ps of
-        2 -> D.PConstr "(,)" (Prelude.map argToPat $ Prelude.map (\p -> A.DArg p) ps)
-        3 -> D.PConstr "(,,)" (Prelude.map argToPat $ Prelude.map (\p -> A.DArg p) ps)
-    A.PListPat lp  -> case lp of
-        LPattern2 p lp' -> cLPat (LPattern5 p lp')
-        LPattern1       -> cLPat LPattern3
-    A.PPat pat     -> case pat of
-        A.Pwild           -> D.PWild
-        (A.PId (Id name)) -> D.PVar name
-        (A.PLit lit)      -> D.PLit (cLit lit)
-        (A.PConst (DConst (TypeId name) _ _)) -> D.PConstr name []
-        (A.PConst (DConst1 (TypeId name)))    -> D.PConstr name []
+argToPat (A.DArg p) = cPattern p
 
-cLPat :: A.LPattern -> D.Pattern
-cLPat LPattern3                = D.PConstr "Nil" []
-cLPat (LPattern4 p)            = D.PConstr "Cons" [(cPat p), (cLPat LPattern3)]
-cLPat (LPattern5 (PPat p) lps) = D.PConstr "Cons" [(cPat p), (cLPat lps)]
+cPattern :: A.Pattern -> D.Pattern
+cPattern p = case p of
+    A.PTuplePat p1 p2     -> D.PConstr "(,)" (Prelude.map argToPat $ Prelude.map (\p -> A.DArg p) [p1,p2])
+    A.PTruplePat p1 p2 p3 -> D.PConstr "(,,)" (Prelude.map argToPat $ Prelude.map (\p -> A.DArg p) [p1,p2,p3])
+    A.PListPat lp         -> cLPat lp
+    A.PWild               -> D.PWild
+    (A.PId (A.Id name))   -> (D.PVar name)
+    (A.PLit l)            -> (D.PLit (cLit l))
+    (A.PConstrEmp (TypeId name)) -> (D.PConstr name [])
+    (A.PCons p1 p2)       -> D.PConstr "Cons" [(cPattern p1), (cPattern p2)]
+    (A.PConsConstr (TypeId name) p1 ps p2) -> D.PConstr "Cons" [(D.PConstr name (map cPattern (p1:ps))), (cPattern p2)]
+    A.PEmpty              -> (D.PConstr "Nil" [])
 
-cPattern :: A.Pattern -> A.Exp -> (D.Pattern, D.Exp)
-cPattern l@(PListPat _) e     = (lPat, (cExp e))
-    where lPat = argToPat (A.DArg l)
-cPattern p@(A.PTuplePat _) e = (tPat, (cExp e))
-    where tPat = argToPat (A.DArg p)
-cPattern (PPat p) e          = ((cPat p), (cExp e))
+cLPat :: A.ListPat -> D.Pattern
+cLPat (PList1 p lp) = D.PConstr "Cons" [(cPattern p), (cLPat lp)]
+cLPat (PList p )    = D.PConstr "Cons" [(cPattern p), (D.PConstr "Nil" [])]
 
--- A pattern from bnfc has no expression bound to it
--- this must be sent from the Def to create the AST pattern
-cPat :: A.Pat -> D.Pattern
-cPat A.Pwild             = D.PWild
-cPat (A.PId (A.Id name)) = (D.PVar name)
-cPat (A.PLit l)          = (D.PLit (cLit l))
-cPat (A.PConst c)        = case c of
-    A.DConst1 (A.TypeId bool)       -> (D.PConstr bool [])
-    A.DConst (A.TypeId bool) id ids -> (D.PConstr bool [])
 
 cType :: A.Type -> D.Exp
 cType (A.TTypeId t) = case t of -- TODO check this part
@@ -138,13 +122,13 @@ cExp (A.EList ls)           = cList ls
 cExp A.EEmptyList           = D.EConstr "Nil"
 cExp (A.ELet lb)            = cLetIn lb
 cExp (A.EApp e1 e2)         = (D.EApp (cExp e1) (cExp e2))
-cExp (A.ELogicalNeg e)      = (D.EUnOp D.Not (cExp e))
-cExp (A.ENeg e)             = (D.EBinOp D.Mul (cExp e) (D.ELit (D.ILit (-1))))
+-- cExp (A.ELogicalNeg e)      = (D.EUnOp D.Not (cExp e))
+-- cExp (A.ENeg e)             = (D.EBinOp D.Mul (cExp e) (D.ELit (D.ILit (-1))))
 cExp (A.EPow e1 e2)         = (D.EBinOp D.Pow (cExp e1) (cExp e2))
 cExp (A.EMul e1 e2)         = (D.EBinOp D.Mul (cExp e1) (cExp e2))
 cExp (A.EDiv e1 e2)         = (D.EBinOp D.Div (cExp e1) (cExp e2))
 cExp (A.EAdd e1 e2)         = (D.EBinOp D.Add (cExp e1) (cExp e2))
-cExp (A.ESub e1 e2)         = (D.EBinOp D.Add (cExp e1) (cExp (A.ENeg e2)))
+--cExp (A.ESub e1 e2)         = (D.EBinOp D.Add (cExp e1) (cExp (A.ENeg e2)))
 cExp (A.ELt e1 e2)          = (D.EBinOp D.Gt (cExp e2) (cExp e1))
 cExp (A.EGt e1 e2)          = (D.EBinOp D.Gt (cExp e1) (cExp e2))
 cExp (A.ELEQ e1 e2)         = (D.EBinOp D.Or (cExp (A.ELt e1 e2)) (cExp (A.EEQ e1 e2)))
@@ -153,8 +137,8 @@ cExp (A.EEQ e1 e2)          = (D.EBinOp D.Eq (cExp e1) (cExp e2))
 cExp (A.ENEQ e1 e2)         = (D.EUnOp D.Not (D.EBinOp D.Eq (cExp e1) (cExp e2)))
 cExp (A.EAND e1 e2)         = D.EUnOp D.Not $ D.EBinOp D.Or (D.EUnOp D.Not (cExp e1)) (D.EUnOp D.Not (cExp e2))
 cExp (A.EOR e1 e2)          = (D.EBinOp D.Or (cExp e1) (cExp e2))
-cExp (A.EBind e1 e2)        = (D.EBinOp D.Bind (cExp e1) (cExp e2))
-cExp (A.Eseq e1 e2)         = (D.EBinOp D.Then (cExp e1) (cExp e2))
+--cExp (A.EBind e1 e2)        = (D.EBinOp D.Bind (cExp e1) (cExp e2))
+--cExp (A.Eseq e1 e2)         = (D.EBinOp D.Then (cExp e1) (cExp e2))
 cExp (A.ECase e cs)         = (D.ECase (cExp e) (cCase cs))
 cExp (A.EIf e1 e2 e3)       = (D.ECase (cExp e1) [((D.PConstr "True" []), (cExp e2)),
                                                   ((D.PConstr "False" []), (cExp e3))])
@@ -196,7 +180,7 @@ cLitList ((ELiteral l):ls) = D.EApp ((D.EApp (D.EConstr "Cons") (D.ELit $ cLit l
 cCase :: A.Cases -> [(D.Pattern, D.Exp)]
 cCase A.ECases3          = []
 cCase (A.ECases1 p e cs) = cCase (A.ECases2 p e cs)
-cCase (A.ECases2 p e cs) = (cPattern p e):(cCase cs)
+cCase (A.ECases2 p e cs) = ((cPattern p),(cExp e)):(cCase cs)
 
 cTuple :: A.Tuple -> D.Exp
 cTuple (Tuple2 e1 e2)    = D.EApp (D.EApp (D.EVar "(,)") (cExp e1)) (cExp e2)
